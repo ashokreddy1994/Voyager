b80a0854d28e241ed652b99f55bc5c563bc2132f
Mulagondla <anusha@dxc.com>
2024-03-09 10:57:48 +0530

    first commit

---
+++ src/test/java/com/test/com/Testerr.java
@@ -0,0 +1,38 @@
+package com.test.com;
+
+import org.testng.annotations.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+public class Testerr {
+
+	@Test
+	public void calculate() throws Exception {
+		System.out.println("Test");
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://google.com");
+		driver.manage().window().maximize();
+		Thread.sleep(5000);
+		driver.findElement(By.name("q")).sendKeys("Automation");
+		Thread.sleep(5000);
+		driver.close();
+		
+	}
+	
+	@Test
+	public void calculate1() throws Exception {
+		System.out.println("Test");
+		WebDriver driver=new EdgeDriver();
+		driver.get("https://google.com");
+		driver.manage().window().maximize();
+		Thread.sleep(5000);
+		driver.findElement(By.name("q")).sendKeys("Selenium");
+		Thread.sleep(5000);
+		driver.close();
+		
+	}
+	
+	
+}
